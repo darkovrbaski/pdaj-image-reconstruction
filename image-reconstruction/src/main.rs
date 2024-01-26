8a905427c6b6c37f568166a1b0ab309435d9ea88
darkovrbaski <vrbaski.ra200.2018@uns.ac.rs>
2024-01-26 01:34:17 +0100

    content aware image load, mse comparison, place best scored piece

--- image-reconstruction/src/main.rs
+++ image-reconstruction/src/main.rs
@@ -2,49 +2,57 @@ use image::{DynamicImage, GenericImage, GenericImageView};
 use image_compare::Algorithm;
 use show_image::{create_window, event, ImageInfo, ImageView};
 use std::{
-    collections::VecDeque,
     fs::{self, File},
-    io::BufReader,
+    io::{BufReader, Read, Seek, SeekFrom},
 };
 
 #[show_image::main]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let pieces_folder_path = "../examples/slika 2/";
-    let image_pieces = load_images_from_folder(pieces_folder_path);
+    let pieces_folder_path = "../examples/slika 1 - 1/";
+    let mut image_pieces = load_images_from_folder(pieces_folder_path);
+    image_pieces.retain(|image| image.width() > 5 && image.height() > 5);
+    println!("image_pieces: {}", image_pieces.len());
 
-    let org_image = image::open("../examples/picture2.jpg").unwrap();
+    let org_image = load_image("../examples/picture1.jpg");
     let mut result_image = blank_image(&org_image);
 
     let mut current_x = 0;
     let mut current_y = 0;
 
-    let mut available_pieces = VecDeque::new();
-    available_pieces.extend(image_pieces);
+    while image_pieces.len() > 0 {
+        let mut match_piece = DynamicImage::new_rgb8(0, 0);
+        let mut match_index = 0;
+        let mut max_score = f64::MAX;
 
-    while let Some(image_piece) = available_pieces.pop_front() {
-        let piece_height = image_piece.height();
-        let sub_img = crop_image(
-            &org_image,
-            current_x,
-            current_y,
-            image_piece.width(),
-            image_piece.height(),
-        );
-        // display_image(&sub_img);
-        // display_image(&image_piece);
+        for (index, image_piece) in image_pieces.iter().enumerate() {
+            let sub_img = crop_image(
+                &org_image,
+                current_x,
+                current_y,
+                image_piece.width(),
+                image_piece.height(),
+            );
+            // display_image(&sub_img);
+            // display_image(&image_piece);
+            let calc_score = compare_images_mse(&sub_img, &image_piece);
 
-        if compare_images_rgb(&sub_img, &image_piece) {
-            place_image(&mut result_image, &image_piece, current_x, current_y);
-            display_image(&result_image);
-
-            current_x += image_piece.width();
-        } else {
-            available_pieces.push_back(image_piece);
+            if max_score > calc_score {
+                match_piece = image_piece.clone();
+                match_index = index;
+                max_score = calc_score;
+                println!("max_score: {}", max_score);
+            }
         }
 
-        if current_x >= result_image.width() - 1 {
+        place_image(&mut result_image, &match_piece, current_x, current_y);
+        current_x += match_piece.width();
+        image_pieces.remove(match_index);
+
+        // display_image(&result_image);
+
+        if current_x >= result_image.width() - 5 {
             current_x = 0;
-            current_y += piece_height;
+            current_y += match_piece.height();
         }
     }
 
@@ -53,15 +61,88 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn crop_image(
-    original: &DynamicImage,
-    crop_x: u32,
-    crop_y: u32,
-    crop_width: u32,
-    crop_height: u32,
-) -> DynamicImage {
-    let cropped_image = original.crop_imm(crop_x, crop_y, crop_width, crop_height);
-    cropped_image
+#[allow(dead_code)]
+fn compare_images_mse(reference_image: &DynamicImage, test_image: &DynamicImage) -> f64 {
+    if reference_image.dimensions() != test_image.dimensions() {
+        return 0.0;
+    }
+
+    // Get the dimensions of the images
+    let (width, height) = test_image.dimensions();
+
+    // Calculate the sum of squared differences
+    let mut sum_squared_diff = 0.0;
+    for y in 0..height {
+        for x in 0..width {
+            let pixel1 = reference_image.to_rgb8().get_pixel(x, y).0;
+            let pixel2 = test_image.to_rgb8().get_pixel(x, y).0;
+
+            for c in 0..pixel1.len() {
+                let diff = f64::from(pixel1[c]) - f64::from(pixel2[c]);
+                sum_squared_diff += diff * diff;
+            }
+        }
+    }
+
+    // Calculate the mean squared error
+    let mse = sum_squared_diff / (height * width) as f64;
+
+    mse
+}
+
+#[allow(dead_code)]
+fn compare_images_px(reference_image: &DynamicImage, test_image: &DynamicImage) -> bool {
+    if reference_image.dimensions() != test_image.dimensions() {
+        return false;
+    }
+
+    for y in 0..reference_image.height() {
+        for x in 0..reference_image.width() {
+            if test_image.to_rgb8().get_pixel(x, y) != reference_image.to_rgb8().get_pixel(x, y) {
+                return false;
+            }
+        }
+    }
+
+    true
+}
+
+#[allow(dead_code)]
+fn compare_images_luma(reference_image: &DynamicImage, test_image: &DynamicImage) -> f64 {
+    let score = image_compare::gray_similarity_structure(
+        &Algorithm::MSSIMSimple,
+        &reference_image.to_luma8(),
+        &test_image.to_luma8(),
+    )
+    .expect("Error: Images had different dimensions")
+    .score;
+    println!("s: {score}");
+    score
+}
+
+#[allow(dead_code)]
+fn compare_images_rgb(reference_image: &DynamicImage, test_image: &DynamicImage) -> f64 {
+    let score = image_compare::rgb_similarity_structure(
+        &Algorithm::MSSIMSimple,
+        &reference_image.to_rgb8(),
+        &test_image.to_rgb8(),
+    )
+    .expect("Error: Images had different dimensions")
+    .score;
+    println!("s: {score}");
+    score
+}
+
+#[allow(dead_code)]
+fn compare_images_his(reference_image: &DynamicImage, test_image: &DynamicImage) -> f64 {
+    let score = image_compare::gray_similarity_histogram(
+        image_compare::Metric::ChiSquare,
+        &reference_image.to_luma8(),
+        &test_image.to_luma8(),
+    )
+    .unwrap();
+    println!("s: {score}");
+    score
 }
 
 fn load_images_from_folder(folder_path: &str) -> Vec<image::DynamicImage> {
@@ -70,20 +151,29 @@ fn load_images_from_folder(folder_path: &str) -> Vec<image::DynamicImage> {
     if let Ok(entries) = fs::read_dir(folder_path) {
         for entry in entries {
             if let Ok(entry) = entry {
-                if let Ok(image) = image::load(
-                    BufReader::new(File::open(entry.path()).unwrap()),
-                    image::ImageFormat::Png,
-                ) {
-                    images.push(image)
-                }
-                // if let Ok(image) = image::open(entry.path()) {
-                //     images.push(image)
-                // }
+                images.push(load_image(&entry.path().to_str().unwrap()));
             }
         }
     }
 
     images
+}
+
+fn load_image(path: &str) -> image::DynamicImage {
+    let file = File::open(path).unwrap();
+    let mut reader = BufReader::new(file);
+
+    // Read the beginning of the file to guess the format
+    let mut start = [0; 16]; // Adjust size as needed
+    reader.read_exact(&mut start).unwrap();
+    let format = image::guess_format(&start).unwrap();
+
+    // Seek back to the start of the file
+    reader.seek(SeekFrom::Start(0)).unwrap();
+
+    // Load the image with the guessed format
+    let image = image::load(reader, format).unwrap();
+    image
 }
 
 fn place_image(dest: &mut DynamicImage, src: &DynamicImage, x: u32, y: u32) {
@@ -100,47 +190,15 @@ fn blank_image(image: &DynamicImage) -> DynamicImage {
     DynamicImage::new_rgb8(image.width(), image.height())
 }
 
-#[allow(dead_code)]
-fn compare_images_px(reference_image: &DynamicImage, test_image: &DynamicImage) -> bool {
-    if reference_image.dimensions() != test_image.dimensions() {
-        return false;
-    }
-
-    for y in 0..reference_image.height() {
-        for x in 0..reference_image.width() {
-            if test_image.get_pixel(x, y) != reference_image.get_pixel(x, y) {
-                return false;
-            }
-        }
-    }
-
-    true
-}
-
-#[allow(dead_code)]
-fn compare_images_luma(reference_image: &DynamicImage, test_image: &DynamicImage) -> bool {
-    let score = image_compare::gray_similarity_structure(
-        &Algorithm::MSSIMSimple,
-        &reference_image.to_luma8(),
-        &test_image.to_luma8(),
-    )
-    .expect("Error: Images had different dimensions")
-    .score;
-    println!("s: {score}");
-    score > 0.70
-}
-
-#[allow(dead_code)]
-fn compare_images_rgb(reference_image: &DynamicImage, test_image: &DynamicImage) -> bool {
-    let score = image_compare::rgb_similarity_structure(
-        &Algorithm::MSSIMSimple,
-        &reference_image.to_rgb8(),
-        &test_image.to_rgb8(),
-    )
-    .expect("Error: Images had different dimensions")
-    .score;
-    println!("s: {score}");
-    score > 0.20
+fn crop_image(
+    original: &DynamicImage,
+    crop_x: u32,
+    crop_y: u32,
+    crop_width: u32,
+    crop_height: u32,
+) -> DynamicImage {
+    let cropped_image = original.crop_imm(crop_x, crop_y, crop_width, crop_height);
+    cropped_image
 }
 
 fn display_image(image: &DynamicImage) {
@@ -154,7 +212,6 @@ fn display_image(image: &DynamicImage) {
 
     for event in window.event_channel().unwrap() {
         if let event::WindowEvent::KeyboardInput(event) = event {
-            println!("{:#?}", event);
             if event.input.key_code == Some(event::VirtualKeyCode::Escape)
                 && event.input.state.is_pressed()
             {
